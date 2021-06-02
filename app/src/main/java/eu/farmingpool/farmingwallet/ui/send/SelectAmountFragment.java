8ee45ec2a6c1968aaefa6524415f45f499deb766
Stefano Rossotti <stefano@sensoriainc.com>
2021-06-03 01:29:04 +0200

    creating SendActivity

---
+++ app/src/main/java/eu/farmingpool/farmingwallet/ui/send/SelectAmountFragment.java
@@ -0,0 +1,22 @@
+package eu.farmingpool.farmingwallet.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import eu.farmingpool.farmingwallet.R;
+
+public class SelectAmountFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_select_amount, container);
+
+        return root;
+    }
+}
